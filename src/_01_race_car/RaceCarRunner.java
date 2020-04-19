93bf06c4cc96e85d71c627140e8d625094a8a46c
root <root@2afa7ba4ef1c>
2020-04-19 18:27:23 +0000

    Yeet

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,14 +11,22 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	RaceCar yeet = new RaceCar("Lambo", 5);
 		// 2. Print the RaceCar's position in the race
-
+System.out.println("Lambo is in 5th place");
 		// 3. Crash the RaceCar
-		
+yeet.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
-		// 5. Help the car move into first place.
+if(yeet.damaged == true) {
+	
+	yeet.pit();
+		
+	// 5. Help the car move into first place.
+	for(int i = yeet.getPositionInRace(); i > 0; i--) {
+	
+		yeet.overtake();
 
 	}
+}
+}
 }
