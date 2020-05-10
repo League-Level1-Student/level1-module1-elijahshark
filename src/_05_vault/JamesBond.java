6f36089d0fa92650a11edbb97a474cfa83361cc3
root <root@1e16ec4fcbae>
2020-05-10 17:56:20 +0000

    yeet

--- src/_05_vault/JamesBond.java
+++ src/_05_vault/JamesBond.java
@@ -4,13 +4,13 @@ public class JamesBond {
 
 	int findCode (Vault yeetith) {
 		for(int i = 0; i < 100000; i++) {
-			
-		if(yeetith.tryCode(100001)) {	
+			System.out.println(i);
+		if(yeetith.tryCode(i)) {	
 		
 		return yeetith.secretcode;
 	
 }
-return -1;
 }
+		return -1;
 }
 }		
--- src/_05_vault/ValutRunner.java
+++ src/_05_vault/ValutRunner.java
@@ -4,8 +4,9 @@ public class ValutRunner {
 
 	public static void main(String[] args) {
 		Vault jerry = new Vault();
+		JamesBond jammys = new JamesBond();
 		System.out.println(jerry.tryCode(100001));
-		
+		System.out.println(jammys.findCode(jerry));
 		
 		
 		
