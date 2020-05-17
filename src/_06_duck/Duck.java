d028fae726db93adde2de0e4b966cb54c545728e
root <root@780913c78277>
2020-05-17 18:25:37 +0000

    Yeet

---
+++ src/_06_duck/Duck.java
@@ -0,0 +1,24 @@
+package _06_duck;
+
+public class Duck {
+int numberOfFriends;
+
+String favoriteFood;
+
+
+Duck(String favoriteFood, int numberOfFriends) {
+  	this.favoriteFood = favoriteFood;
+  	this.numberOfFriends = numberOfFriends;
+}
+
+
+void quack() {
+System.out.println("yeet");
+
+}
+void waddle() {
+	System.out.println("yeetith");
+
+	
+}
+}
---
+++ src/_06_duck/DuckRunner.java
@@ -0,0 +1,17 @@
+package _06_duck;
+
+public class DuckRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Duck jerry = new Duck();
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
