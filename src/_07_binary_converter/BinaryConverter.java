8e7068c4733910a1d403dcc86952d8a78f73cb9b
root <root@5dec17be5f65>
2020-08-09 18:26:53 +0000

    Yeet

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -1,5 +1,25 @@
 package _07_binary_converter;
 
-public class BinaryConverter {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-}
+public class BinaryConverter {
+	public void run() {
+
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JTextField answer = new JTextField(20);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	}
---
+++ src/_07_binary_converter/BinaryConverterRunner.java
@@ -0,0 +1,8 @@
+package _07_binary_converter;
+
+
+public class BinaryConverterRunner {
+	public static void main(String[] args) {
+		new BinaryConverter().run();
+	}
+}
