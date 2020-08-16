dbee1624d851e6e867d3e86463be6399ba75e34b
root <root@ee83c4e9ab27>
2020-08-16 18:25:48 +0000

    yeeeeert

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -1,6 +1,8 @@
 package _07_binary_converter;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -10,8 +12,19 @@ public class BinaryConverter {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JTextField answer = new JTextField(20);
+		JButton button = new JButton();
+		JLabel label = new JLabel();
 	
 	
+	
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(panel);
+	panel.add(answer);
+	frame.setSize(400,80);
+	panel.add(button);
+	button.setText("Convert");
+	panel.add(label);
 	
 	
 	
