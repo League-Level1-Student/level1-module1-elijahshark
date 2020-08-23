c2e29c1a700a9995061ff43e07eb17c3ca0baf57
root <root@a8aa5a8396a7>
2020-08-23 18:03:32 +0000

    yeet

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -1,20 +1,26 @@
 package _07_binary_converter;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BinaryConverter {
-	public void run() {
-
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+public class BinaryConverter implements ActionListener {
 		JTextField answer = new JTextField(20);
 		JButton button = new JButton();
 		JLabel label = new JLabel();
-	
+		public void run() {
+
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		
+		
+		
 	
 	
 	frame.setVisible(true);
@@ -25,7 +31,26 @@ public class BinaryConverter {
 	panel.add(button);
 	button.setText("Convert");
 	panel.add(label);
+	button.addActionListener(this);
+	answer.addActionListener(this);
 	
+	
+	
+	
+	
+	
+	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JButton buttonClicked = (JButton) arg0.getSource();
+		
+		if(button == buttonClicked) {
+		String converter = convert(answer.getText());
+		label.setText(converter);
+		}
 	
 	
 	
@@ -35,4 +60,25 @@ public class BinaryConverter {
 	
 	
 	}
+	String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+
+	
 	}
